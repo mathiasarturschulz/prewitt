7b02d4ee17cad5e542f96df0d1710c6d125d1506
Mathias Schulz <mathias@schulz.net.br>
2021-12-06 22:21:18 -0300

    [ADD] #prewitt - Adicionado versao inicial funcional

--- prewitt.py
+++ prewitt.py
@@ -1,4 +1,6 @@
 import cv2
+import math
+import numpy as np
 from datetime import datetime
 
 class Prewitt:
@@ -19,10 +21,45 @@ class Prewitt:
         Returns:
             string: String com o conteúdo da imagem
         """
-        # realiza a leitura da imagem
+        prewittx = [
+            [-1, 0, 1],
+            [-1, 0, 1],
+            [-1, 0, 1]
+        ]
+        prewitty = [
+            [1, 1, 1],
+            [0, 0, 0],
+            [-1, -1, -1]
+        ]
+
+        # realiza a leitura da imagem já em tons de cinza
         img = cv2.imread(self.imageName, cv2.IMREAD_GRAYSCALE)
 
-        return img
+        # busca a largura e altura da imagem
+        width, height = img.shape
+
+        pixels = np.zeros((int(width), int(height)))
+        print('pixels')
+        print(pixels)
+
+        linScale = .3
+
+        #For each pixel in the image
+        for row in range(width-len(prewittx)):
+            for col in range(height-len(prewittx)):
+                Gx = 0
+                Gy = 0
+                for i in range(len(prewittx)):
+                    for j in range(len(prewitty)):
+                        val = img[row+i, col+j] * linScale
+                        Gx += prewittx[i][j] * val
+                        Gy += prewitty[i][j] * val
+
+                pixels[(row+1),(col+1)] = int(math.sqrt(Gx*Gx + Gy*Gy))
+
+        print('pixels')
+        print(pixels)
+        return pixels
 
     def __filename(self):
         """Método responsável por gerar o nome da imagem
